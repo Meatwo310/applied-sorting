19a22fe7adfa0ce43749f4b6cd9c76cc97f5bd0d
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-08-22 21:09:57 +0900

    naming

---
+++ src/main/java/io/github/meatwo310/appliedsorting/AppliedSorting.java
@@ -0,0 +1,12 @@
+package io.github.meatwo310.appliedsorting;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(AppliedSorting.MODID)
+public class AppliedSorting {
+    public static final String MODID = "appliedsorting";
+
+    public AppliedSorting(FMLJavaModLoadingContext context) {
+    }
+}
